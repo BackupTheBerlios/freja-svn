08a73e018bbfdc60764e3d40ed3852039d08a044
troelskn <troelskn@f1f2e4fc-880e-0410-8948-cad986e913d6>
2006-04-18 21:55:26 +0000

    nitpicking

--- trunk/examples/basecamp_api/proxy/php/proxy.php
+++ trunk/examples/basecamp_api/proxy/php/proxy.php
@@ -1,12 +1,12 @@
 <?php
-	
+
 	$login    = "freja";
 	$password = "apitest";
 	$baseUrl  = "http://freja.projectpath.com/";
-	
+
 	$HTTPMethod     = $_GET['HTTP_REQUEST_METHOD'];
 	$HTTPRequestUrl = $_GET['HTTP_REQUEST_URL'];
-	
+
 	include('Request.php');
 
 	// safety check, only request legit url
@@ -14,16 +14,16 @@
 
 		$req = &new HTTP_Request($HTTPRequestUrl);
 		$req->setBasicAuth($login,$password);
-		$req->setMethod($HTTPMethod);	
+		$req->setMethod($HTTPMethod);
 		$req->addHeader('Content-Type','application/xml');
 		$req->addHeader('Accept','application/xml');
-		$req->clearPostData();		
+		$req->clearPostData();
 		// $req->addPostData('Foo', 'bar');
 		$req->sendRequest();
-		
+
 		$response = $req->getResponseBody();
 		$code     = $req->getResponseCode();
-		
+
 		switch($code) {
 			case 200:
 				header("HTTP/1.0 200 Ok");
@@ -33,11 +33,14 @@
 				break;
 			case 404:
 				header("HTTP/1.0 404 Not Found");
-				break;				
+				exit;
+			defaul:
+				header("HTTP/1.0 502 Bad Gateway");
+				exit;
 		}
 		header('Content-Type: text/xml');
 		echo $response;
-	} 
+	}
 	else {
 		header("HTTP/1.0 403 Forbidden");
 		echo "Requested url not allowed.";
